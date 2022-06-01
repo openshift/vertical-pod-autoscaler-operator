e8fcb4ae93507fd93f4b27607f796fc870ad20c9
D. Paolella <dpaolell@redhat.com>
2022-06-01 17:28:14 +0200

    Update golang to 1.18 in gofmt

--- hack/go-fmt.sh
+++ hack/go-fmt.sh
@@ -18,6 +18,6 @@ else
     --env IS_CONTAINER=TRUE \
     --volume "${PWD}:/go/src/github.com/openshift/${REPO_NAME}:z" \
     --workdir "/go/src/github.com/openshift/${REPO_NAME}" \
-    openshift/origin-release:golang-1.15 \
+    openshift/origin-release:golang-1.18 \
     ./hack/go-fmt.sh "${@}"
 fi
