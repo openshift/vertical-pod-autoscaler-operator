96f2ac4149939b9d5e727f82c1bfb8299151201a
John Kyros <jkyros@redhat.com>
2024-02-08 16:11:26 -0600

    Update go version in Dockerfile,Makefile,lint,vet
    There are some spots in these scripts where we directly referece the
    golang version.
    
    This updates those scripts to reference go version 1.19 -> 1.21.

--- hack/go-vet.sh
+++ hack/go-vet.sh
@@ -7,6 +7,6 @@ else
     --env IS_CONTAINER=TRUE \
     --volume "${PWD}:/go/src/github.com/openshift/${REPO_NAME}:z" \
     --workdir "/go/src/github.com/openshift/${REPO_NAME}" \
-    registry.ci.openshift.org/openshift/release:golang-1.19 \
+    registry.ci.openshift.org/openshift/release:golang-1.21 \
     ./hack/go-vet.sh "${@}"
 fi;
